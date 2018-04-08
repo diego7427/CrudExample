64464b75f6bcdb06aae80a5e0cfb87d68ab807f6
diego <diego@JUANDIEGOGA8BEC>
2018-04-09 00:39:15 +0200

    Divide into modules. Fix the multiple empty notes error

---
+++ crud-app/src/main/resources/static/js/app/CustomerController.js
@@ -0,0 +1,156 @@
+'use strict';
+
+angular.module('crudApp').controller('CustomerController',
+    ['CustomerService', '$scope',  function( CustomerService, $scope) {
+
+        var self = this;
+        self.customer = {};
+        self.customers=[];
+        self.statusCombo={};
+
+        self.submit = submit;
+        self.getAllCustomers = getAllCustomers;
+        self.createCustomer = createCustomer;
+        self.updateCustomer = updateCustomer;
+        self.removeCustomer = removeCustomer;
+        self.editCustomer = editCustomer;
+        self.reset = reset;
+        self.getAllComboValues = getAllComboValues;
+        self.getEdition = getEdition;
+        self.addNote=addNote;
+        self.removeNote=removeNote;
+
+        self.successMessage = '';
+        self.errorMessage = '';
+        self.done = false;
+        self.note = '';
+
+        self.onlyIntegers = /^\d+$/;
+        self.onlyNumbers = /^\d+([,.]\d+)?$/;
+        self.edit=false;
+        
+      
+
+        function submit() {
+            console.log('Submitting');
+            if (self.customer.id === undefined || self.customer.id === null) {
+                console.log('Saving New Customer', self.customer);
+                createCustomer(self.customer);
+            } else {
+                updateCustomer(self.customer, self.customer.id);
+                console.log('Customer updated with id ', self.customer.id);
+            }
+        }
+
+        function createCustomer(customer) {
+            console.log('About to create customer');
+            customer.notes=[];
+            var newNote={};
+            newNote.description=self.note;
+            customer.notes.push(newNote);
+            CustomerService.createCustomer(customer)
+                .then(
+                    function (response) {
+                        console.log('Customer created successfully');
+                        self.successMessage = 'Customer created successfully';
+                        self.errorMessage='';
+                        self.done = true;
+                        self.customer={};
+                        self.note = '';
+                        $scope.myForm.$setPristine();
+                    },
+                    function (errResponse) {
+                        console.error('Error while creating Customer');
+                        self.errorMessage = 'Error while creating Customer: ' + errResponse.data.errorMessage;
+                        self.successMessage='';
+                    }
+                );
+        }
+        
+        
+
+
+        function updateCustomer(customer, id){
+            console.log('About to update customer');
+            CustomerService.updateCustomer(customer, id)
+                .then(
+                    function (response){
+                        console.log('Customer updated successfully');
+                        self.successMessage='Customer updated successfully';
+                        self.errorMessage='';
+                        self.done = true;
+                        reset();
+                        $scope.myForm.$setPristine();
+                    },
+                    function(errResponse){
+                        console.error('Error while updating Customer');
+                        self.errorMessage='Error while updating Customer '+errResponse.data;
+                        self.successMessage='';
+                    }
+                );
+        }
+
+
+        function removeCustomer(id){
+            console.log('About to remove Customer with id '+id);
+            CustomerService.removeCustomer(id)
+                .then(
+                    function(){
+                        console.log('Customer '+id + ' removed successfully');
+                    },
+                    function(errResponse){
+                        console.error('Error while removing customer '+id +', Error :'+errResponse.data);
+                    }
+                );
+        }
+
+
+        function getAllCustomers(){
+            return CustomerService.getAllCustomers();
+        }
+        
+
+        function editCustomer(id) {
+            self.successMessage='';
+            self.errorMessage='';
+            CustomerService.getCustomer(id).then(
+                function (customer) {
+                    self.customer = customer;
+                    self.edit=true;
+                },
+                function (errResponse) {
+                    console.error('Error while removing customer ' + id + ', Error :' + errResponse.data);
+                }
+            );
+        }
+        function reset(){
+            self.successMessage='';
+            self.errorMessage='';
+            self.customer={};
+            self.note = '';
+            self.edit=false;
+            $scope.myForm.$setPristine(); //reset Form
+        }
+        
+        function getAllComboValues() {
+            return CustomerService.getAllComboValues();
+        }
+        function getEdition(){
+            return self.edit;
+        }
+        
+        function addNote(){
+            self.customer.notes.push({
+              id: '',
+              description: '',
+            });
+          }
+          function removeNote(index){
+              self.customer.notes.splice(index, 1);
+              $scope.myForm.$pristine=false;
+          }
+        
+    }
+
+
+    ]);
---
+++ crud-repository/src/main/java/nz/co/propellerhead/springboot/model/Note.java
@@ -0,0 +1,135 @@
+/**
+ * Status.java 06-abr-2018
+ *
+ * Copyright 2018 INDITEX.
+ * Departamento de Sistemas
+ */
+package nz.co.propellerhead.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * The Class Note.
+ *
+ * @author <a href="${email}">${author}</a>
+ */
+@Entity(name = "CUSTOMER_NOTE")
+@Table(name = "CUSTOMER_NOTE")
+public class Note implements Serializable {
+
+    private static final long serialVersionUID = -4206812344438397365L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "CUSTOMER_ID")
+    private Customer customer;
+
+    /**
+     * Obtiene id.
+     *
+     * @return id
+     */
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * Establece id.
+     *
+     * @param id
+     *            nuevo id
+     */
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Obtiene description.
+     *
+     * @return description
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Establece description.
+     *
+     * @param description
+     *            nuevo description
+     */
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    /**
+     * Obtiene customer.
+     *
+     * @return customer
+     */
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    /**
+     * Establece customer.
+     *
+     * @param customer
+     *            nuevo customer
+     */
+    public void setCustomer(final Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object other) {
+        if (!(other instanceof Note)) {
+            return false;
+        }
+        final Note castOther = (Note) other;
+        return new EqualsBuilder().append(this.id, castOther.id).append(this.description, castOther.description)
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this.id).append(this.description).toHashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("id", this.id).append("description", this.description).toString();
+    }
+
+}
